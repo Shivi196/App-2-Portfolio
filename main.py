04e86c03f70ba34c7af15321fd52e2d199d73abd
misha <misha07.sharma@gmail.com>
2025-01-22 19:49:40 +0530

    Create columns,img and description

---
+++ main.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+col1,col2 = st.columns(2)
+
+with col1:
+
+    col1= st.image("images/photo.jpeg")
+
+with col2:
+
+    # st.title(" Shivi Sharma ")
+
+    st.markdown("<h1 style='color:Darkblue;'>Shivi Sharma</h1>", unsafe_allow_html=True)
+    content = ''' 
+    Results-oriented Software Engineer with strong SQL, Java, Python, and data warehousing skills. Experience in Supply Chain Management (OMP) and Finance (AITRA). Developed and maintained applications for logistics optimization and AR/AP reconciliation. Focus on delivering robust and scalable solutions."
+     '''
+    col2 = st.info(content)
