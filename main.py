5853d00acc9d5092fbad141a4aed8f710d7edf51
misha <misha07.sharma@gmail.com>
2025-01-22 20:10:24 +0530

    Add horizontal lines and content below image and description

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-col1,col2 = st.columns(2)
+col1,col2= st.columns(2)
+
 
 with col1:
 
@@ -10,8 +11,14 @@ with col2:
 
     # st.title(" Shivi Sharma ")
 
-    st.markdown("<h1 style='color:Darkblue;'>Shivi Sharma</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='color:DarkBlue;'>Shivi Sharma</h1>", unsafe_allow_html=True)
     content = ''' 
     Results-oriented Software Engineer with strong SQL, Java, Python, and data warehousing skills. Experience in Supply Chain Management (OMP) and Finance (AITRA). Developed and maintained applications for logistics optimization and AR/AP reconciliation. Focus on delivering robust and scalable solutions."
      '''
     col2 = st.info(content)
+
+st.markdown("---")
+content2 = '''
+                Below mentioned are some Apps I have built in Python. Feel Free to contact me!!!
+                '''
+st.write(content2)
