13117ba1be1a729be61ae8eac1b637770762e9bd
misha <misha07.sharma@gmail.com>
2025-01-22 23:32:27 +0530

    Add the apps description,images and links with extra space btw cols

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ with col2:
     content = ''' 
     Results-oriented Software Engineer with strong SQL, Java, Python, and data warehousing skills. Experience in Supply Chain Management (OMP) and Finance (AITRA). Developed and maintained applications for logistics optimization and AR/AP reconciliation. Focus on delivering robust and scalable solutions."
      '''
-    col2 = st.info(content)
+    col2 = st.info(content) #you can also use st.write() bt info will highlight this in diff way..
+
 
 # This will add new horizontal line below image and description
 st.markdown("---")
@@ -29,12 +30,18 @@ st.write(content2)
 # Why we are using pandas.. bcoz pandas will give help you in reading csv along with it will give you data in tabluar format
 # you can use with open() to read csv file but that will return content in one single string then you need to use string methods to part ways the data in desired format it will be length process
 
-col3,col4 = st.columns(2)
+col3,emp_col,col4 = st.columns([1.5,0.5,1.5])
+
 df = pandas.read_csv("data.csv",sep=";")
 with col3:
     for index,row in df[:10].iterrows():
         st.header(row["title"])
-
+        st.write(row["description"])
+        st.image(f"images/{row["image"]}")
+        st.write(f"[Source Code]({row["url"]})")
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image(f"images/{row["image"]}")
+        st.write(f"[Source Code]({row['url']})")
