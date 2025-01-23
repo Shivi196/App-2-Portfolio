dbb46fefcf23fd90b798b67784c7c230138135a6
misha <misha07.sharma@gmail.com>
2025-01-23 19:28:49 +0530

    Add Contact us page along with send emails using the form

--- Home.py
+++ Home.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas
 
+
+st.set_page_config(layout="wide")
+
 col1,col2= st.columns(2)
 
 # with is used to access the data of the columns
@@ -12,12 +15,12 @@ with col2:
 
     # st.title(" Shivi Sharma ")
     # This will change color of title using html in python code...
-    st.markdown("<h1 style='color:DarkBlue;'>Shivi Sharma</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='color:White;'>Shivi Sharma</h1>", unsafe_allow_html=True)
     content = ''' 
     Results-oriented Software Engineer with strong SQL, Java, Python, and data warehousing skills. Experience in Supply Chain Management (OMP) and Finance (AITRA). Developed and maintained applications for logistics optimization and AR/AP reconciliation. Focus on delivering robust and scalable solutions."
      '''
-    col2 = st.info(content) #you can also use st.write() bt info will highlight this in diff way..
-
+    # col2 = st.info(content) #you can also use st.write() bt info will highlight this in diff way..
+    col2 = st.write(content)
 
 # This will add new horizontal line below image and description
 st.markdown("---")
@@ -33,15 +36,18 @@ st.write(content2)
 col3,emp_col,col4 = st.columns([1.5,0.5,1.5])
 
 df = pandas.read_csv("data.csv",sep=";")
+
 with col3:
     for index,row in df[:10].iterrows():
         st.header(row["title"])
         st.write(row["description"])
         st.image(f"images/{row["image"]}")
         st.write(f"[Source Code]({row["url"]})")
+
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
         st.write(row["description"])
         st.image(f"images/{row["image"]}")
         st.write(f"[Source Code]({row['url']})")
+
---
+++ send_email.py
@@ -0,0 +1,23 @@
+import smtplib,ssl
+
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    context = ssl.create_default_context()
+    sender_email = "sharmabruno310@gmail.com"
+    sender_password = "ahbw faam tral qbgo"
+    user_email = "sharmabruno310@gmail.com"
+
+#     message = '''\
+# Subject : Test Email
+# In the context of secure communication, an SSL Context is a collection of settings and configurations that define the parameters for establishing a secure connection using SSL/TLS protocols.
+# '''
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(sender_email, sender_password)
+        server.sendmail(sender_email, user_email, message)
+
+
+
+
+
